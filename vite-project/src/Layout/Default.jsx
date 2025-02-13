22130fe5ddcd1662a4b8b69dac017db6620f67a1
MirkoDD <mirkodidio111@gmail.com>
2025-02-13 16:42:19 +0100

    fixed:links_working

--- vite-project/src/Layout/Default.jsx
+++ vite-project/src/Layout/Default.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Home from "../Pages/Home";
-import Header from "../Components/Header";
+import Header from "../Sections/Header";
 import List from "../Pages/List";
 // import Main from "../Components/Main";
 
@@ -8,6 +8,7 @@ export default function DefaultLayout() {
   return (
     <div>
       <Header />
+      <Outlet />
       {/* <Main /> */}
     </div>
   );
